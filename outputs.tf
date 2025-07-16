fceed64e6b3c1de670077c74ea281beed6fe54f1
Paul Yu <paul.d.yu@gmail.com>
2025-07-16 13:20:22 -0700

    fix: remove unnecessary outputs for Kubernetes cluster and update container registry outputs

--- outputs.tf
+++ outputs.tf
@@ -11,17 +11,12 @@ output "resource_id" {
   value       = azurerm_kubernetes_cluster.this.id
 }
 
-output "kubernetes_cluster_name" {
-  description = "The name of the Kubernetes cluster."
-  value       = azurerm_kubernetes_cluster.this.name
+output "container_registry_resource" {
+  description = "This is the full output for the container registry resource."
+  value       = azurerm_container_registry.this
 }
 
 output "container_registry_resource_id" {
-  description = "The `azurerm_container_registry`'s resource id."
-  value       = azurerm_container_registry.this.login_server
-}
-
-output "container_registry_login_server" {
-  description = "The login server of the container registry."
-  value       = azurerm_container_registry.this.login_server
+  description = "The `azurerm_kubernetes_cluster`'s resource id."
+  value       = azurerm_container_registry.this.id
 }
